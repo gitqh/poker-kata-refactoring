b084fc5638832349927968089b629b91f9d27ad0
gitqh <quhang19881208@163.com>
2023-07-03 19:44:21 +0800

    refactor: add needed fields in Hand

--- src/main/java/com/thoughtworks/refactor/Hand.java
+++ src/main/java/com/thoughtworks/refactor/Hand.java
@@ -1,4 +1,10 @@
 package com.thoughtworks.refactor;
 
 public class Hand {
+    private String type;
+    private String numbers;
+    private int index;
+    private int[] arraySort;
+    private int[] repeat;
+    private int[] noRepeat;
 }
