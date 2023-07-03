3602ef33d98fb28f98152b12f1c703a6d65c56a5
gitqh <quhang19881208@163.com>
2023-07-03 19:48:43 +0800

    refactor: move judgeType to Hand

--- src/main/java/com/thoughtworks/refactor/Hand.java
+++ src/main/java/com/thoughtworks/refactor/Hand.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.refactor;
 
 import java.util.Arrays;
+import java.util.HashSet;
 
 public class Hand {
     private String type;
@@ -45,6 +46,52 @@ public class Hand {
             renumber[i] = number[number.length - i - 1];
         }
         return renumber;
+    }
+
+    //判断是什么牌
+    static String judgeType(String str) {
+        String type = "";
+        String[] strArray = str.split("");
+        int[] number = strNumber(str);
+        int i;
+        String[] color = new String[5];
+        for (i = 0; i < 5; i++) {
+            color[i] = strArray[i * 3 + 1];
+        }
+        HashSet<Integer> hashSetNumber = new HashSet<Integer>();
+        for (i = 0; i < 5; i++) {
+            hashSetNumber.add(number[i]);
+        }
+        HashSet<String> hashSetType = new HashSet<String>();
+        for (i = 0; i < 5; i++) {
+            hashSetType.add(color[i]);
+        }
+        if (hashSetNumber.size() == 5) {
+            if ((number[0] - number[4] == 4) && (hashSetType.size() == 1) && (hashSetNumber.size() == 5)) { //五个相邻的数字且花色一样——同花顺
+                type = "StraightFlush";
+            } else if (number[0] - number[4] == 4 && (hashSetNumber.size() == 5)) { //五个相邻数字——顺子
+                type = "Straight";
+            } else if (hashSetType.size() == 1) { //同一花色——同花
+                type = "Flush";
+            } else { //五个不相邻的数字——散牌
+                type = "HighCard";
+            }
+        } else if (hashSetNumber.size() == 4) { //一对相同，其余三个数字不同——对子
+            type = "OnePair";
+        } else if (hashSetNumber.size() == 3) {
+            if ((number[0] == number[1] && number[2] == number[3]) || (number[1] == number[2] && number[3] == number[4]) || (number[0] == number[1] && number[3] == number[4])) { //两对
+                type = "TwoPair";
+            } else { //三个数字相同，另外两个数字不同——三条
+                type = "ThreeOfAKind";
+            }
+        } else {
+            if (number[0] != number[1] || number[3] != number[4]) { //三个数字相同，另外两个数字相同——葫芦
+                type = "FourOfAKind";
+            } else { //四个数字相同——铁支
+                type = "FullHouse";
+            }
+        }
+        return type;
     }
 
     public String getType() {
--- src/main/java/com/thoughtworks/refactor/Poker.java
+++ src/main/java/com/thoughtworks/refactor/Poker.java
@@ -7,14 +7,14 @@ public class Poker {
     private static final String[] CARD_TYPES = new String[]{"StraightFlush", "FourOfAKind", "FullHouse", "Flush", "Straight", "ThreeOfAKind", "TwoPair", "OnePair", "HighCard"};
 
     public String compareResult(String black, String white) {
-        String blackType = judgeType(black);
+        String blackType = Hand.judgeType(black);
         int[] blackNumber = Hand.strNumber(black);
         int blackIndex = judgeIndex(blackType);
         int[] blackArraySort = arraySort(blackNumber);
         int[] blackRepeat = noOrRepeatNumber(blackNumber, 0);
         int[] blackNoRepeat = noOrRepeatNumber(blackNumber, 1);
 
-        String whiteType = judgeType(white);
+        String whiteType = Hand.judgeType(white);
         int[] whiteNumber = Hand.strNumber(white);
         int whiteIndex = judgeIndex(whiteType);
         int[] whiteArraySort = arraySort(whiteNumber);
@@ -247,52 +247,6 @@ public class Poker {
             }
         }
         return index;
-    }
-
-    //判断是什么牌
-    private String judgeType(String str) {
-        String type = "";
-        String[] strArray = str.split("");
-        int[] number = Hand.strNumber(str);
-        int i;
-        String[] color = new String[5];
-        for (i = 0; i < 5; i++) {
-            color[i] = strArray[i * 3 + 1];
-        }
-        HashSet<Integer> hashSetNumber = new HashSet<Integer>();
-        for (i = 0; i < 5; i++) {
-            hashSetNumber.add(number[i]);
-        }
-        HashSet<String> hashSetType = new HashSet<String>();
-        for (i = 0; i < 5; i++) {
-            hashSetType.add(color[i]);
-        }
-        if (hashSetNumber.size() == 5) {
-            if ((number[0] - number[4] == 4) && (hashSetType.size() == 1) && (hashSetNumber.size() == 5)) { //五个相邻的数字且花色一样——同花顺
-                type = "StraightFlush";
-            } else if (number[0] - number[4] == 4 && (hashSetNumber.size() == 5)) { //五个相邻数字——顺子
-                type = "Straight";
-            } else if (hashSetType.size() == 1) { //同一花色——同花
-                type = "Flush";
-            } else { //五个不相邻的数字——散牌
-                type = "HighCard";
-            }
-        } else if (hashSetNumber.size() == 4) { //一对相同，其余三个数字不同——对子
-            type = "OnePair";
-        } else if (hashSetNumber.size() == 3) {
-            if ((number[0] == number[1] && number[2] == number[3]) || (number[1] == number[2] && number[3] == number[4]) || (number[0] == number[1] && number[3] == number[4])) { //两对
-                type = "TwoPair";
-            } else { //三个数字相同，另外两个数字不同——三条
-                type = "ThreeOfAKind";
-            }
-        } else {
-            if (number[0] != number[1] || number[3] != number[4]) { //三个数字相同，另外两个数字相同——葫芦
-                type = "FourOfAKind";
-            } else { //四个数字相同——铁支
-                type = "FullHouse";
-            }
-        }
-        return type;
     }
 
 }
