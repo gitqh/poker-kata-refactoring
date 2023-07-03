15b8cd7f2e1153ece529a98af9289ae67770733f
gitqh <quhang19881208@163.com>
2023-07-03 21:17:39 +0800

    refactor: remove unused method compareOnePair

--- src/main/java/com/thoughtworks/refactor/Poker.java
+++ src/main/java/com/thoughtworks/refactor/Poker.java
@@ -75,32 +75,6 @@ public class Poker {
         return winResult;
     }
 
-    private String compareOnePair(final Hand blackHand, final Hand whiteHand) {
-        String winResult = "";
-        if (blackHand.getRepeat()[0] < whiteHand.getRepeat()[0]) {
-            String sig = intNumber(whiteHand.getRepeat()[0]);
-            winResult = WHITE_WINS_HINTS + sig;
-        } else if (blackHand.getRepeat()[0] > whiteHand.getRepeat()[0]) {
-            String sig = intNumber(blackHand.getRepeat()[0]);
-            winResult = BLACK_WINS_HINTS + sig;
-        } else {
-            for (int i = 0; i < 3; i++) {
-                if (blackHand.getNoRepeat()[i] < whiteHand.getNoRepeat()[i]) {
-                    String sig = intNumber(whiteHand.getNoRepeat()[i]);
-                    winResult = WHITE_WINS_HINTS + sig;
-                    break;
-                } else if (blackHand.getNoRepeat()[i] > whiteHand.getNoRepeat()[i]) {
-                    String sig = intNumber(blackHand.getNoRepeat()[i]);
-                    winResult = BLACK_WINS_HINTS + sig;
-                    break;
-                } else {
-                    winResult = "tie";
-                }
-            }
-        }
-        return winResult;
-    }
-
     protected String intNumber(int i) {
         String[] strNumber = {"2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"};
         return strNumber[i - 2];
