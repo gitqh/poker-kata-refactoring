ef07a4d2f16631e2775154daa1103b83956911fb
gitqh <quhang19881208@163.com>
2023-07-03 21:13:31 +0800

    refactor: remove unused method compareThreeOfAKind

--- src/main/java/com/thoughtworks/refactor/Poker.java
+++ src/main/java/com/thoughtworks/refactor/Poker.java
@@ -126,18 +126,6 @@ public class Poker {
         return winResult;
     }
 
-    private String compareThreeOfAKind(final Hand blackHand, final Hand whiteHand) {
-        String winResult;
-        if (blackHand.getRepeat()[0] < whiteHand.getRepeat()[0]) {
-            String sig = intNumber(whiteHand.getRepeat()[0]);
-            winResult = WHITE_WINS_HINTS + sig;
-        } else {
-            String sig = intNumber(blackHand.getRepeat()[0]);
-            winResult = BLACK_WINS_HINTS + sig;
-        }
-        return winResult;
-    }
-
     protected String intNumber(int i) {
         String[] strNumber = {"2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"};
         return strNumber[i - 2];
