d8e9da3186dcd48a128b2a633abdc1c6635dc82a
gitqh <quhang19881208@163.com>
2023-07-03 21:06:25 +0800

    refactor: create sub class FlushPoker

---
+++ src/main/java/com/thoughtworks/refactor/FlushPoker.java
@@ -0,0 +1,22 @@
+package com.thoughtworks.refactor;
+
+public class FlushPoker extends Poker{
+    @Override
+    protected String compare(final Hand blackHand, final Hand whiteHand) {
+        String winResult = "";
+        for (int i = 0; i < 5; i++) {
+            if (blackHand.getNumbers()[i] < whiteHand.getNumbers()[i]) {
+                String sig = intNumber(whiteHand.getNumbers()[i]);
+                winResult = WHITE_WINS_HINTS + sig;
+                break;
+            } else if (blackHand.getNumbers()[i] > whiteHand.getNumbers()[i]) {
+                String sig = intNumber(blackHand.getNumbers()[i]);
+                winResult = BLACK_WINS_HINTS + sig;
+                break;
+            } else {
+                winResult = "tie";
+            }
+        }
+        return winResult;
+    }
+}
